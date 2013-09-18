cd1509e823fc3eebd8342b87d89f806da0489fbd
root <root@ewn3.skytruth.org>
2013-09-18 18:08:02 +0000

    Support geolocation and tags

--- hooks/flickr.php
+++ hooks/flickr.php
@@ -13,8 +13,20 @@ class Flickr {
         $api = new phpFlickr(Settings_Model::get_setting('flickr_key'), Settings_Model::get_setting('flickr_secret'));
         $api->setToken(Settings_Model::get_setting('flickr_token'));
 
+        $tags = array();
+        foreach($update->incident_category as $incident_category) {
+            array_push($tags, str_replace(" ", "-", $incident_category->category->category_title));
+        }
+        $tags = implode(" ", $tags);
+
+        $lat = $update->location->latitude;
+        $lon = $update->location->longitude;
+        $title = $update->incident_title;
+        $desc = $update->incident_description;
+
         foreach($update->media as $media) {
-            $api->async_upload('media/uploads/' . $media->media_link, $update->incident_title);
+            $photoid = $api->sync_upload('media/uploads/' . $media->media_link, $title, $desc, $tags, true);
+            $api->photos_geo_setLocation($photoid, $lat, $lon);
         }
     }
 }
